50670b8e48a570164aaa55a8ef551583ee5978b4
tatojastine-n <jajanicoletato@gmail.com>
2025-08-08 22:01:34 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWordLength
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(sentence))
+            {
+                Console.WriteLine("Error: Empty input");
+                return;
+            }
+            int maxLength = 0;
+            int currentWordLength = 0;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] != ' ')
+                {
+                    currentWordLength++;
+                }
+                else
+                {
+                    if (currentWordLength > maxLength)
+                    {
+                        maxLength = currentWordLength;
+                    }
+                    currentWordLength = 0;
+                }
+            }
+            // Check the last word in case sentence doesn't end with space
+            if (currentWordLength > maxLength)
+            {
+                maxLength = currentWordLength;
+            }
+            Console.WriteLine($"Length of the longest word: {maxLength}");
+        }
+    }
+}
